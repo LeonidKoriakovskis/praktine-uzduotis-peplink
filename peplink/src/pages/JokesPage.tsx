8c0d446093b5574ae75fa4ab7c0cb11292c0a708
LeonidKoriakovskis <leonidas0727@gmail.com>
2025-06-12 20:00:28 +0300

    added navbar to navigate through pages

---
+++ peplink/src/pages/JokesPage.tsx
@@ -0,0 +1,10 @@
+function JokesPage() {
+  return (
+    <div>
+      <h1>Jokes Page</h1>
+      <p>This is the jokes page.</p>
+    </div>
+  );
+}
+
+export default JokesPage;
---
+++ peplink/src/pages/UsersPage.tsx
@@ -0,0 +1,10 @@
+function UsersPage() {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      <p>This is the Users page content.</p>
+    </div>
+  );
+}
+
+export default UsersPage;
